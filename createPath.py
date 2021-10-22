b580257ceba5cd704cb33d8fe2ab2aa818493954
HatGuy68 <irahul.2k@gmail.com>
2021-10-22 16:07:44 +0530

    first prototype

--- createPath.py
+++ createPath.py
@@ -1,4 +1,6 @@
+import cv2
 import json
+from pathsolver import solver
 
 f = open('testRoom.json',)
 data = json.load(f)
@@ -7,7 +9,18 @@ for i in data:
     doors.append(i)
 f.close()
 
-print(doors)
+# print(doors)
+
+frame=cv2.imread("dept.jpg")
+frame=cv2.resize(frame, (800,400))
+frame=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+th,frame=cv2.threshold(frame,200,255,cv2.THRESH_BINARY)
+frame=cv2.bitwise_not(frame)
+
+def draw_route(frame, route):
+    for r in route:
+        frame[r[0]][r[1]] = 255
+    return frame
 
 def generatePairs(doors):
     pairs = []
@@ -19,17 +32,32 @@ def generatePairs(doors):
 pairs = generatePairs(doors)
 
 def generatePaths(pairs):
+    solve = solver()
     paths = {}
     for pair in pairs:
-        print(pair[0])
-        path = generatePath(pair)
+        path = generatePath(pair, solve)
         paths[tuple([eval(pair[0]),eval(pair[1])])] = path
     return paths
 
-def generatePath(pair):
+def generatePath(pair, solve):
+    start = eval(pair[0])
+    end = eval(pair[1])
+    print('checking')
     
-    # To be Coded
+    route=solve.astar(start,end,frame)
+    if(route==False):
+        print("No path")
+        return 0
+    route+=[start]
+    route=route[::-1]
     
-    return pair
+    return route
 
-print(generatePaths(pairs))
+paths = generatePaths(pairs)
+
+for path in paths.values():
+    draw_route(frame, path)
+    
+cv2.imshow( "frame", frame)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
