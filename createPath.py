5661f75eaaf2cd3758d5b20625761ce0e0c6b4ed
HatGuy68 <irahul.2k@gmail.com>
2021-11-22 09:02:34 +0530

    Ho gaya bas

--- createPath.py
+++ createPath.py
@@ -1,8 +1,9 @@
 import cv2
 import json
+import pandas as pd
 from pathsolver import solver
 
-f = open('testRoom.json',)
+f = open('Rooms.json',)
 data = json.load(f)
 doors = []
 for i in data:
@@ -18,6 +19,7 @@ th,frame=cv2.threshold(frame,200,255,cv2.THRESH_BINARY)
 frame=cv2.bitwise_not(frame)
 
 def draw_route(frame, route):
+    if route == 0: return
     for r in route:
         frame[r[0]][r[1]] = 150
     return frame
@@ -53,7 +55,26 @@ def generatePath(pair, solve):
     
     return route
 
+def createDataFrame(paths):
+    source_pd = []
+    dest_pd = []
+    paths_pd = []
+    for key in paths.keys():
+        source_pd.append(key[0])
+        dest_pd.append(key[1])
+    for val in paths.values():
+        paths_pd.append(val)
+        
+    df = {"sources": source_pd, "destinations": dest_pd, "paths": paths_pd}
+    dataframe = pd.DataFrame(df, index=None)
+    dataframe.to_json('Paths.json')
+    print(dataframe)
+    
+    
+
+
 paths = generatePaths(pairs)
+createDataFrame(paths)
 
 for path in paths.values():
     draw_route(frame, path)
